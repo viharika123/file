bf2fd0f1445db266b7912bbbcfa286536f2689e4
viharika123 <viharikabhavanasi@gmail.com>
2020-08-04 11:16:25 -0500

    Updates

---
+++ precache-manifest.65511c047f1d3dd7fb67fc56a81c704e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b0bf3aceaf6c800f28cb29546b697d",
+    "url": "/file/index.html"
+  },
+  {
+    "revision": "86938229a13fb5514038",
+    "url": "/file/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb6ffb3945c9bd6953b3",
+    "url": "/file/static/js/2.72442180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/file/static/js/2.72442180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86938229a13fb5514038",
+    "url": "/file/static/js/main.07ca4d69.chunk.js"
+  },
+  {
+    "revision": "089e3f57b1b7879e41b5",
+    "url": "/file/static/js/runtime-main.acf0437b.js"
+  }
+]);
